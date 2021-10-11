c6c721f66a1d4bb865279e1e2c1315dff57574e0
raahi102 <rit2014009@iiita.ac.in>
2021-10-11 22:45:55 +0530

    Updates

---
+++ precache-manifest.80f599960a4d49177c8b35ebe256134f.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f9d9c38e1cb28fe9739cbbfb1d4adf0",
+    "url": "/MagFlux/index.html"
+  },
+  {
+    "revision": "ab11a92a7b7ed4057465",
+    "url": "/MagFlux/static/css/main.f304e9c0.chunk.css"
+  },
+  {
+    "revision": "eaeb0bc70805121ee319",
+    "url": "/MagFlux/static/js/2.b3ea6a10.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MagFlux/static/js/2.b3ea6a10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab11a92a7b7ed4057465",
+    "url": "/MagFlux/static/js/main.dc1916bd.chunk.js"
+  },
+  {
+    "revision": "7561210131d58d384149",
+    "url": "/MagFlux/static/js/runtime-main.170b3e86.js"
+  },
+  {
+    "revision": "af9a76add1d63319abcf702c17201fb6",
+    "url": "/MagFlux/static/media/HelveticaNeue.af9a76ad.woff"
+  },
+  {
+    "revision": "190b4e095db9510029445bb90080aa77",
+    "url": "/MagFlux/static/media/HelveticaNeueBold.190b4e09.woff"
+  },
+  {
+    "revision": "0a13c540938b1b7dd3996b02ea568e5f",
+    "url": "/MagFlux/static/media/HelveticaNeueMedium.0a13c540.ttf"
+  },
+  {
+    "revision": "9a9a6ae7c5ecadd780246eecfe86f9e9",
+    "url": "/MagFlux/static/media/HelveticaNeueMediumItalic.9a9a6ae7.woff"
+  },
+  {
+    "revision": "9899e63559782f635520be97eb948ed7",
+    "url": "/MagFlux/static/media/activityBasedLearning@2x.9899e635.jpg"
+  },
+  {
+    "revision": "74ba830dd3400031f0d4f969e82ee753",
+    "url": "/MagFlux/static/media/approach@2x.74ba830d.png"
+  },
+  {
+    "revision": "d4ffaeee3328886d0a3c196b12f7218f",
+    "url": "/MagFlux/static/media/approachSmall@2x.d4ffaeee.png"
+  },
+  {
+    "revision": "0fadf605f3f3dbddd5d6adf067315249",
+    "url": "/MagFlux/static/media/arrow.0fadf605.png"
+  },
+  {
+    "revision": "eac9eea208f2c79d6b96a6fbf1928392",
+    "url": "/MagFlux/static/media/bulb.eac9eea2.png"
+  },
+  {
+    "revision": "9b4f113bd711df4e369e070f1ff9965d",
+    "url": "/MagFlux/static/media/clock.9b4f113b.png"
+  },
+  {
+    "revision": "622d4a1fdbefd231f70e4a946820c71d",
+    "url": "/MagFlux/static/media/communicationSkills@2x.622d4a1f.jpg"
+  },
+  {
+    "revision": "b2f7f984aa331ba5ed3ea9b14f704122",
+    "url": "/MagFlux/static/media/gear.b2f7f984.png"
+  },
+  {
+    "revision": "ed704e0fc679485c24c54c2d236b113b",
+    "url": "/MagFlux/static/media/hero2x.ed704e0f.jpg"
+  },
+  {
+    "revision": "6af2549c72c8138793a695ae4cac6505",
+    "url": "/MagFlux/static/media/heroSmall2x.6af2549c.jpg"
+  },
+  {
+    "revision": "0908398610be4453e947b35f5ed8aa29",
+    "url": "/MagFlux/static/media/logo1024.09083986.jpeg"
+  },
+  {
+    "revision": "4982a08235af0cf049b9082727275e34",
+    "url": "/MagFlux/static/media/pencil.4982a082.png"
+  },
+  {
+    "revision": "6e667587e708be7a23c5c830165e4a2e",
+    "url": "/MagFlux/static/media/personalityDevelopment@2x.6e667587.jpg"
+  },
+  {
+    "revision": "6ae31bfddb7f0312426a4b2aa3a5bb0e",
+    "url": "/MagFlux/static/media/rope.6ae31bfd.png"
+  }
+]);
